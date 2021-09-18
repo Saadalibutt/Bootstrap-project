5108378e38d37abe1a3a0afa1b7237522ec15205
Saadalibutt <saadalibutt072@gmail.com>
2021-09-18 15:45:08 +0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,32 @@
+let name="Saad";
+let lName="Ali";
+
+console.log("my name is "+ name +' '+lName);
+console.log(name +' '+ lName);
+
+const array=['Saad','Ali','Arslan'];
+console.log(array);
+console.log(array[2]);
+array[3]='Subhan';
+console.log(array);
+
+for(i=0;i<4;i++){
+
+console.log(array[i]);
+
+
+}
+
+
+function myfunction(){
+
+document.getElementById("jsFunc").innerHTML="JS Function Called ";
+
+
+
+}
+alert(1+9);
+
+console.log(10+5);
+
+
